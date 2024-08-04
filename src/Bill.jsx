13272033d0a64b7d80e51332c98db2ee4065fffd
Kasim-bigshorts <Kasim.Kadiwala@bigshorts.co>
2024-08-04 08:50:26 +0530

    added cng logo

--- src/Bill.jsx
+++ src/Bill.jsx
@@ -22,7 +22,7 @@ const Bill = forwardRef((props, ref) => {
     return (
         <div ref={ref} className="py-2 px-4 max-w-4xl mx-auto border border-gray-300">
             <div className="flex flex-col w-full gap-3 justify-between items-center pb-4">
-                <div className='w-full text-center items-center flex flex-col gap-3'>
+                <div className='w-full text-center items-center flex flex-col gap-2'>
                     <img src="/logo.png" alt="" className='object-cover w-96 mt-1' />
                     <p className='italic font-semibold'>New Online CNG Kit Fitting & Cylinder Retesting</p>
                     <p className='topBorder font-bold p-1 w-full bottomBorder'>G72 Apple Plaza , Sidhpur , Patan - 384290 (Gujarat)</p>
@@ -34,7 +34,7 @@ const Bill = forwardRef((props, ref) => {
 
             <div className="topBorder leftBorder rightBorder pl-1">
                 <div className="flex justify-between">
-                    <div className=''>
+                    <div className='flex flex-col justify-between py-1'>
                         <p><span className="font-semibold uppercase">Bill To:</span> {billTo}</p>
                         <p><span className="font-semibold uppercase">Mob:</span> {mobileNo}</p>
                         <p><span className="font-semibold uppercase">Add:</span> {address}</p>
